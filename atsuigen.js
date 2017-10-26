c243758f793da3c3544fb84ec905397883378397
threegirls89 <threegirls89.asm@gmail.com>
2017-10-27 01:25:00 +0900

    背景色変更機能を追加

--- atsuigen.js
+++ atsuigen.js
@@ -17,9 +17,8 @@ function draw(){
 	var bottomMargin = 40;
 	var rightMargin = 40;
 	var factorialSep = 20;
-		
 
-
+// 文字列サイズ取得
 	context.font = "bold 120px 'メイリオ'";
 	var metrics = context . measureText(str);
 	var strWidth = metrics.width;
@@ -34,17 +33,33 @@ function draw(){
 	var strStartx = (canvas.width - leftMargin - rightMargin - textWidth) / 2 + leftMargin;
 	var strStarty = (canvas.height - topMargin - bottomMargin - textHeight) / 2 + textHeight + topMargin;
 
-
+// 文字描画
 	context.font = "bold 120px 'メイリオ'";
 	context.fillStyle = "#ff0000";
 	context.fillText(str, strStartx, strStarty);
 	context.font = "italic bold 120px 'メイリオ'";
 	context.fillText("!", strStartx + strWidth + factorialSep, strStarty);
 
-	context.fillStyle = "#fdfbd4";
-	context.fillRect(0, 0, canvas.width, strStarty - topMargin - textHeight);
 
-	context.fillStyle = "#35c42e";
+// 背景描画
+	var listbox = document.getElementById("lstBackground");
+	switch(listbox.value){
+		case "nii2":
+			upperColor = "#fdfbd4";
+			lowerColor = "#35c42e";
+			break;
+		case "shonan":
+			upperColor = "#354F33";
+			lowerColor = "#354F33";
+			break;
+		default:
+			upperColor = "#fdfbd4";
+			lowerColor = "#35c42e";
+			break;
+	}
+	context.fillStyle = upperColor;
+	context.fillRect(0, 0, canvas.width, strStarty - topMargin - textHeight);
+	context.fillStyle = lowerColor;
 	context.fillRect(0, strStarty + bottomMargin, canvas.width, canvas.height - strStarty - bottomMargin);
 }
 
